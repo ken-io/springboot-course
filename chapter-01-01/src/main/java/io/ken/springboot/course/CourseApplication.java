10db0757d39969b033eeb86cd6f15bc526af99cb
ken <mail@keshuang.net>
2017-08-17 15:56:50 +0800

    使用Spring Boot构建第一个Web应用程序

---
+++ chapter-01-01/src/main/java/io/ken/springboot/course/CourseApplication.java
@@ -0,0 +1,12 @@
+package io.ken.springboot.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+}
