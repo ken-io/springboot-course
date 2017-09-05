2ba2c3c39cba8affeb2cc177edf3be2bbb91b3aa
ken <mail@keshuang.net>
2017-09-05 18:00:33 +0800

    使用Spring Boot+Thymeleaf模板引擎开发Web应用

---
+++ chapter-03-02/src/main/java/io/ken/springboot/course/controller/HelloController.java
@@ -0,0 +1,24 @@
+package io.ken.springboot.course.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/welcome")
+    ModelAndView welcome(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("welcome");
+        modelAndView.addObject("message","Welcome to Spring Boot & Thymeleaf");
+        return modelAndView;
+    }
+}
